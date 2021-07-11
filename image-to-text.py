c4b8fe92dd1a659abe70f8a718e97e3210572c03
Yi Xiong Ng <yixiongng@gmail.com>
2021-07-12 01:02:52 +0800

    Commented displaying image

--- image-to-text.py
+++ image-to-text.py
@@ -24,41 +24,45 @@ gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 threshold_img = cv2.threshold(gray_image, 0, 255, cv2.THRESH_OTSU)[1]
 
-# display image
-resizedimage = imutils.resize(threshold_img, width=700)
-cv2.imshow('threshold image', resizedimage)
+### uncomment it if you want to show the image
+# # display image
+# resizedimage = imutils.resize(threshold_img, width=700)
+# cv2.imshow('threshold image', resizedimage)
 
-# Maintain output window until user presses a key
-cv2.waitKey(0)
+# # Maintain output window until user presses a key
+# cv2.waitKey(0)
 
-# Destroying present windows on screen
-cv2.destroyAllWindows()
+# # Destroying present windows on screen
+# cv2.destroyAllWindows()
+### uncomment until here
+
 #configuring parameters for tesseract
-
 custom_config = r'--oem 3 --psm 6'
 
 # now feeding image to tesseract
 
 details = pytesseract.image_to_data(threshold_img, output_type=Output.DICT, config=custom_config, lang='eng')
 
-# print(details.keys())
 total_boxes = len(details['text'])
 
-for sequence_number in range(total_boxes):
+### uncomment it if you want to show the image
+# for sequence_number in range(total_boxes):
 
-	if int(float(details['conf'][sequence_number])) > 30:
-		(x, y, w, h) = (details['left'][sequence_number], details['top'][sequence_number], details['width'][sequence_number],  details['height'][sequence_number])
-		threshold_img = cv2.rectangle(threshold_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+# 	if int(float(details['conf'][sequence_number])) > 30:
+# 		(x, y, w, h) = (details['left'][sequence_number], details['top'][sequence_number], details['width'][sequence_number],  details['height'][sequence_number])
+# 		threshold_img = cv2.rectangle(threshold_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-# display image
-resizedimage = imutils.resize(threshold_img, width=700)
-cv2.imshow('captured text', resizedimage)
+# # display image
+# resizedimage = imutils.resize(threshold_img, width=700)
+# cv2.imshow('captured text', resizedimage)
 
-# Maintain output window until user presses a key
-cv2.waitKey(0)
+# # Maintain output window until user presses a key
+# cv2.waitKey(0)
 
-# Destroying present windows on screen
-cv2.destroyAllWindows()
+# # Destroying present windows on screen
+# cv2.destroyAllWindows()
+### uncomment until here
+
 parse_text = []
 word_list = []
 last_word = ''
